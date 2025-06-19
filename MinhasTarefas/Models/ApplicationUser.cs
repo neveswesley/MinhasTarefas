6273e4fb442cc4d10b062ab0eb3c1850fcab9805
wesley neves <wellynvs@gmail.com>
2025-06-19 00:03:49 -0300

    initial commit

---
+++ MinhasTarefas/Models/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Identity;
+
+namespace MinhasTarefas.Models;
+
+public class ApplicationUser : IdentityUser
+{
+    public string FullName { get; set; }
+    
+    [ForeignKey("UsuarioId")]
+    public virtual ICollection<Tarefa> Tarefas { get; set; }
+    
+    [ForeignKey("UsuarioId")]
+    public virtual ICollection<Token> Tokens { get; set; }
+    
+}
---
+++ MinhasTarefas/Repositories/Interfaces/ITokenRepository.cs
@@ -0,0 +1,11 @@
+﻿using MinhasTarefas.Models;
+
+namespace MinhasTarefas.Repositories.Interfaces;
+
+public interface ITokenRepository
+{
+    void CadastrarToken(Token token);
+    Token ObterToken(string refreshToken);
+    void AtualizarToken(Token token);
+
+}
